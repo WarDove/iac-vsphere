b1010a53287ad30ecbfd8683d4de711a9351e336
Tarlan Huseynov <tarlan.huseynov@rabita.az>
2022-08-26 16:26:54 +0400

    hooray

--- Terraform/backend.tf
+++ Terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   required_version = ">=1.2.6"
   backend "s3" {
-    region = "eu-central-1"
+    region  = "eu-central-1"
     profile = "terraform"
-    key = "terraform.tfstate"
-    bucket = "azin-infra-dev-state"
+    key     = "terraform.tfstate"
+    bucket  = "azin-infra-dev-state"
   }
 }
--- Terraform/main.tf
+++ Terraform/main.tf
@@ -62,6 +62,49 @@ data "vsphere_resource_pool" "target-resource-pool" {
   ]
 }
 
+data "vsphere_virtual_machine" "template" {
+  name          = "linux-ubuntu-desktop-amd64-22-04-lts"
+  datacenter_id = vsphere_datacenter.azin-sphere-dc.moid
+  depends_on = [
+    vsphere_host.esx-01
+  ]
+}
+
+# HOLY MOLY
+
+resource "vsphere_virtual_machine" "vm" {
+  name             = "hello-world"
+  resource_pool_id = data.vsphere_resource_pool.target-resource-pool.id
+  datastore_id     = data.vsphere_datastore.main-datastore.id
+  num_cpus         = 1
+  memory           = 1024
+  guest_id         = data.vsphere_virtual_machine.template.guest_id
+  scsi_type        = data.vsphere_virtual_machine.template.scsi_type
+  network_interface {
+    network_id   = data.vsphere_network.vm-network.id
+    adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
+  }
+  disk {
+    label            = "disk0"
+    size             = data.vsphere_virtual_machine.template.disks.0.size
+    thin_provisioned = data.vsphere_virtual_machine.template.disks.0.thin_provisioned
+  }
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+    customize {
+      linux_options {
+        host_name = "hello-world"
+        domain    = "example.com"
+      }
+      network_interface {
+        ipv4_address = "10.180.12.170"
+        ipv4_netmask = 24
+      }
+      ipv4_gateway = "10.180.12.20"
+    }
+  }
+}
+
 # # Create VM
 # resource "vsphere_virtual_machine" "test" {
 #   name             = "test"
