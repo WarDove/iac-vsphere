9a8c0d10b8f7f891801ae0c1ea2395c47b6e614e
Tarlan Huseynov <tarlan.huseynov@rabita.az>
2022-08-26 17:02:13 +0400

    updated

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,76 +1,50 @@
-# Create the DC
-resource "vsphere_datacenter" "azin-sphere-dc" {
-  name = var.dc_name
-}
-
-# Add underlying hosts into DC
-resource "vsphere_host" "esx-01" {
-  hostname   = var.esx-01_server
-  username   = var.esx-01_user
-  password   = var.esx-01_password
-  datacenter = vsphere_datacenter.azin-sphere-dc.moid
-
-  lifecycle {
-    ignore_changes = [
-      cluster
-    ]
-  }
-
-}
-
-# # Add Compute Cluster
-# resource "vsphere_compute_cluster" "azin-sphere-cc" {
-#   name            = "azin-sphere-compute-cluster"
-#   datacenter_id   = vsphere_datacenter.azin-sphere-dc.moid
-#   host_system_ids = [vsphere_host.esx-01.id]
-
-#   drs_enabled = false
-#   ha_enabled  = false
+# # Create the DC
+# resource "vsphere_datacenter" "azin-sphere-dc" {
+#   name = var.dc_name
 # }
 
-# # Add Resource Pool if DRS & HA Enabled 
-#  resource "vsphere_resource_pool" "azin-sphere-rp" {
-#    name                    = "azin-sphere-rp"
-#    parent_resource_pool_id = vsphere_compute_cluster.azin-sphere-cc.resource_pool_id
-#  }
+# # Add underlying hosts into DC
+# resource "vsphere_host" "esx-01" {
+#   hostname   = var.esx-01_server
+#   username   = var.esx-01_user
+#   password   = var.esx-01_password
+#   datacenter = vsphere_datacenter.azin-sphere-dc.moid
 
+#   lifecycle {
+#     ignore_changes = [
+#       cluster
+#     ]
+#   }
+# }
+
+data "vsphere_datacenter" "main-dc" {
+  name = "azin-sphere-dc"
+}
 
 # Get Datastore info from vcenter
 data "vsphere_datastore" "main-datastore" {
   name          = "main-datastore"
-  datacenter_id = vsphere_datacenter.azin-sphere-dc.moid
-  depends_on = [
-    vsphere_host.esx-01
-  ]
+  datacenter_id = data.vsphere_datacenter.main-dc.id
 }
 
 # Get Network info from vcenter
 data "vsphere_network" "vm-network" {
   name          = "VLAN3513"
-  datacenter_id = vsphere_datacenter.azin-sphere-dc.moid
-  depends_on = [
-    vsphere_host.esx-01
-  ]
+  datacenter_id = data.vsphere_datacenter.main-dc.id
 }
 
 # Target Resource Pool
 data "vsphere_resource_pool" "target-resource-pool" {
   name          = "${var.esx-01_server}/Resources/"
-  datacenter_id = vsphere_datacenter.azin-sphere-dc.moid
-  depends_on = [
-    vsphere_host.esx-01
-  ]
+  datacenter_id = data.vsphere_datacenter.main-dc.id
 }
 
 data "vsphere_virtual_machine" "template" {
   name          = "linux-ubuntu-desktop-amd64-22-04-lts"
-  datacenter_id = vsphere_datacenter.azin-sphere-dc.moid
-  depends_on = [
-    vsphere_host.esx-01
-  ]
+  datacenter_id = data.vsphere_datacenter.main-dc.id
 }
 
-# HOLY MOLY
+# Provision a VM
 
 resource "vsphere_virtual_machine" "vm" {
   name             = "hello-world"
@@ -80,6 +54,7 @@ resource "vsphere_virtual_machine" "vm" {
   memory           = 1024
   guest_id         = data.vsphere_virtual_machine.template.guest_id
   scsi_type        = data.vsphere_virtual_machine.template.scsi_type
+
   network_interface {
     network_id   = data.vsphere_network.vm-network.id
     adapter_type = data.vsphere_virtual_machine.template.network_interface_types[0]
@@ -105,19 +80,3 @@ resource "vsphere_virtual_machine" "vm" {
   }
 }
 
-# # Create VM
-# resource "vsphere_virtual_machine" "test" {
-#   name             = "test"
-#   resource_pool_id = data.vsphere_resource_pool.target-resource-pool.id 
-#   datastore_id     = data.vsphere_datastore.main-datastore.id
-#   num_cpus         = 1
-#   memory           = 1024
-#   guest_id         = "centos7_64Guest"
-#   network_interface {
-#     network_id = data.vsphere_network.vm-network.id
-#   }
-#   disk {
-#     label = "disk0"
-#     size  = 20
-#   }
-# }
